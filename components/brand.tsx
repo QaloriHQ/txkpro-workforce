1f6700757c2e4c9d7c475f59a6884a750dab4a02
Qalori <team.qalori@gmail.com>
2026-09-15 18:58:58 -0500

    Use TXKPRO logo assets in platform brand

--- components/brand.tsx
+++ components/brand.tsx
@@ -1,10 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export function Brand() {
   return (
-    <Link className="brand" href="/">
-      <span className="brand-mark">TXK</span>
-      <span className="brand-word">PRO</span>
+    <Link className="brand" href="/" aria-label="TXKPRO Workforce home">
+      <span className="brand-logo-wrap" aria-hidden="true">
+        <Image className="brand-logo brand-logo-light" src="/txkpro-logo-light.svg" alt="" width={150} height={38} priority />
+        <Image className="brand-logo brand-logo-dark" src="/txkpro-logo-dark.svg" alt="" width={150} height={38} priority />
+      </span>
       <span className="brand-sub">Workforce</span>
     </Link>
   );
