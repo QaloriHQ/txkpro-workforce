9cf7b079e3ecf99d1a0e8675dd1b2a117a4a3728
Qalori <team.qalori@gmail.com>
2026-09-23 16:43:51 -0500

    Use official TXKPRO light and dark brand assets

--- components/brand.tsx
+++ components/brand.tsx
@@ -5,8 +5,22 @@ export function Brand() {
   return (
     <Link className="brand" href="/" aria-label="TXKPRO Workforce home">
       <span className="brand-logo-wrap" aria-hidden="true">
-        <Image className="brand-logo brand-logo-light" src="/txkpro-logo-light.svg" alt="" width={150} height={38} priority />
-        <Image className="brand-logo brand-logo-dark" src="/txkpro-logo-dark.svg" alt="" width={150} height={38} priority />
+        <Image
+          className="brand-logo brand-logo-light"
+          src="/brand/txkpro-logo-light.png"
+          alt=""
+          width={700}
+          height={174}
+          priority
+        />
+        <Image
+          className="brand-logo brand-logo-dark"
+          src="/brand/txkpro-logo-dark.png"
+          alt=""
+          width={700}
+          height={171}
+          priority
+        />
       </span>
       <span className="brand-sub">Workforce</span>
     </Link>
